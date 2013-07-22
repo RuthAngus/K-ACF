acaf0db15be5147b0d7ce5b9e9512615f685df83
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2013-07-22 15:16:41 +0100

    Star spot simulation code

---
+++ spot_sim_ruth5.py
@@ -0,0 +1,265 @@
+# This is the version that reads in Kepler data, uses its time sampling and adds star spots to it.
+# It does individual quarters
+# This version produces star spots from the entire Kepler lc, then adds to individual quarters
+
+
+import scipy
+import scipy.io
+import pylab
+import numpy
+import glob
+import pyfits
+
+ROOTDIR = '/Users/angusr/angusr/ACF/star_spot_sim'
+
+def mklc(nspot = 200, incl = (scipy.pi)*5./12., amp = 0.01, \
+         tau = 1.5, diffrot = 0.0, \
+         dur = 20.0, samp = 0.01, noise = 0.001, doplot = False, myperiod = [10.0], Amplitude = 1.0, quarter = 3):
+
+         # spots.py calls runSim which calls mklc
+    
+    ''' This is a simplified version of the class-based routines in
+    spot_model.py. It generates a light curves for dark, point like
+    spots with no limb-darkening. 
+
+    Parameters:
+    nspot = desired number of spots present on star at any
+            one time
+    amp = desired light curve amplitude
+    tau = characteristic spot life-time
+    dur = light curve duration
+    samp = time-sampling
+    diffrot = fractional difference between equatorial and polar
+              rotation period
+    doplot: set to True to produce plots
+    (unit of time is equatorial rotation period)'''
+
+    # IMPORT KEPLER LIGHTCURVE
+
+    name = '006370489'
+    time2 = []; lightcurve = []
+    quarter_times2 = []
+    quarter_times_start = []
+    quarter_times_fin = []
+    files = glob.glob('/Users/angusr/angusr/data2/all_Qs/kplr%s-*llc.fits' % (name))
+    #print len(files)
+    for f in range(0,12):
+        hdulist = pyfits.open(files[f])
+        tbdata = hdulist[1].data #"add the first extension to tbdata"
+        x = numpy.where(numpy.isfinite(tbdata['TIME']))
+        time = tbdata['TIME'][x]
+        lc = tbdata['PDCSAP_FLUX'][x]
+        x = numpy.where(numpy.isfinite(tbdata['PDCSAP_FLUX']))
+        time = tbdata['TIME'][x]
+        lc = tbdata['PDCSAP_FLUX'][x]
+        quarter_times2.append(tbdata['TIME'])
+        quarter_times_start.append(time[0])
+        quarter_times_fin.append(time[-1])
+        lightcurve.extend(numpy.array(lc))
+        time2.extend(numpy.array(time))
+
+    
+    
+    myperiod = float(myperiod[0])
+    print 'Period = ', myperiod
+    dur = (max(time2) - min(time2))
+
+
+    # SET UP THE SPOTS
+    print 'Setting up the spots...'
+
+    # (crude estimate of) total number of spots needed during entire
+    # time-series
+    nspot_tot = int(nspot * dur / 2 / tau)
+    # uniform distribution of spot longitudes
+    lon = scipy.rand(nspot_tot) * 2 * scipy.pi 
+   # distribution of spot latitudes uniform in sin(latitude)
+    lat = scipy.arcsin(scipy.rand(nspot_tot)) 
+    # spot rotation rate optionally depends on latitude
+
+
+
+    
+    period = ((scipy.sin(lat) - 0.5) * diffrot + 1.0 )*myperiod
+
+    
+    period0 = scipy.ones(nspot_tot)*myperiod
+    # all spots have the same maximum area
+    # (crude estimate of) filling factor needed per spot
+    ff = amp / scipy.sqrt(nspot)
+    scale_fac = 1
+    amax = scipy.ones(nspot_tot) * ff * scale_fac
+    # all spots have the evolution timescale
+    decay = scipy.ones(nspot_tot) * tau
+    # uniform distribution of spot peak times 
+    # start well before and end well after time-series limits (to
+    # avoid edge effects)
+    extra = 3 * decay.max()
+    pk = scipy.rand(nspot_tot) * (dur + 2 * extra) - extra
+
+    # COMPUTE THE LIGHT CURVE
+    print 'Computing the light curve...'
+    
+
+    time = numpy.array(time2- min(time2))
+    addit = min(time2)
+
+    
+    npt = len(time)
+    area_tot = scipy.zeros(npt)
+    dF_tot = scipy.zeros(npt)
+    dF_tot0 = scipy.zeros(npt)
+
+   
+    
+    
+    
+    
+    # add up the contributions of individual spots
+    for i in range(nspot_tot):
+        # Spot area
+        if (pk[i] == 0) + (decay[i] == 0):
+            area = scipy.ones(npt) * amax[i]
+        else:
+            area = amax[i] * \
+                scipy.exp(-(time - pk[i])**2 / 2. / decay[i]**2)
+        area_tot += area
+        # Fore-shortening 
+        phase = 2 * scipy.pi * time / period[i] + lon[i]
+        phase0 = 2 * scipy.pi * time / period0[i] + lon[i]
+        mu = scipy.cos(incl) * scipy.sin(lat[i]) + \
+            scipy.sin(incl) * scipy.cos(lat[i]) * scipy.cos(phase)
+        mu0 = scipy.cos(incl) * scipy.sin(lat[i]) + \
+            scipy.sin(incl) * scipy.cos(lat[i]) * scipy.cos(phase0)
+        mu[mu < 0] = 0.0
+        mu0[mu0 < 0] = 0.0
+        # Flux
+        dF_tot -= area * mu
+        dF_tot0 -= area * mu0
+
+   
+    
+    print 'Adding noise...'
+    # ADD NOISE
+    noi = pylab.normal(0, 1, npt) * noise
+    dF_tot += noi
+    dF_tot0 += noi
+        
+    amp_eff = dF_tot.max()-dF_tot.min()
+    nspot_eff = area_tot / scale_fac / ff
+
+    if doplot == True:
+        print 'Used %d spots in total over %d rotation periods.' \
+            % (nspot_tot, dur)
+        print 'Mean filling factor of individual spots was %.4f.' \
+            % ff
+        print 'Desired amplitude was %.4f, actual amplitude was %.4f.' \
+            % (amp, amp_eff)
+        print 'Desired number of spots at any one time was %d.' % nspot
+        print 'Actual number of spots was %d (min), %d (average), %d (max).' \
+
+        pylab.close(1)
+        pylab.figure(1, (10, 4))
+        xoff = 0.1
+        xoffr = 0.05
+        yoff = 0.13
+        yoffr = 0.07
+        xwi = (1.0 - xoff - xoffr)
+        ywi = (1.0 - yoff - yoffr) / 2
+        xll = xoff 
+        yll = 1.0 - yoffr - ywi
+        ax1 = pylab.axes([xll, yll, xwi, ywi])
+        pylab.plot(time, area_tot * 100, 'k-')
+        #pylab.title('i=%.1f <Ns>=%d A=%.4f Fs=%.4f tau=%.2fP sig=%.4f diff=%.1f' \
+        #            % (incl * 180 / scipy.pi, nspot_eff.mean(), amp_eff, ff, tau, noise, diffrot))
+        #pylab.title('i=%.1f <Ns>=%d tau=%.2fP Period = %d days' \
+        #            % (incl * 180 / scipy.pi, nspot_eff.mean(), tau, myperiod)) 
+        pylab.ylabel('fill. fac. (%)')
+        yll = 1.0 - yoffr - 2 * ywi
+        axc = pylab.axes([xll, yll, xwi, ywi], sharex = ax1)
+        pylab.plot(time, 1 + dF_tot, 'k-', label = 'diff rot')
+        pylab.plot(time, 1 + dF_tot0 - amp_eff, 'r-', label = 'no diff rot')
+        pylab.ylabel('rel. flux')
+        pylab.xlabel('time (days)')
+        pylab.xlim(time.min(), time.max())
+        pylab.legend()
+        #pylab.savefig('/Users/angusr/angusr/ACF/star_spot_sim', )
+
+    
+    
+
+    
+
+    # Split into quarters
+        
+    print 'quarter = ', quarter
+    time = time + addit
+    #print 'start = ', quarter_times_start[quarter]
+    #print 'stop = ', quarter_times_fin[quarter]
+    x = numpy.where(time == quarter_times_start[quarter])
+    y = numpy.where(time == quarter_times_fin[quarter])
+
+    start = int(x[0])
+    stop = int(y[0])
+    
+    time = time[start:stop]
+    area_tot = area_tot[start:stop]
+    dF_tot = dF_tot[start:stop]
+    dF_tot0 = dF_tot0[start:stop]
+    lightcurve = lightcurve[start:stop]
+
+    # Add normalised Kepler lc to simulated lc
+    lightcurve = lightcurve/numpy.median(lightcurve)
+    lightcurve = lightcurve - numpy.median(lightcurve)
+    dF_tot = dF_tot - numpy.median(dF_tot)
+    dF_tot0 = dF_tot0 - numpy.median(dF_tot0)
+    #dF_tot = (dF_tot)*numpy.median(lightcurve) + lightcurve
+    #dF_tot0 = (dF_tot0)*numpy.median(lightcurve) + lightcurve
+    # Normalise
+    #dF_tot = dF_tot/numpy.median(dF_tot)
+    #dF_tot0 = dF_tot0/numpy.median(dF_tot0)
+
+    npt = len(time)
+
+    res0 = scipy.array([nspot_eff.mean(), ff, amp_eff, noise])
+    res1 = scipy.zeros((4, npt))
+
+    pylab.close(2)
+    pylab.figure(2)
+    pylab.subplot(3,1,1)
+    pylab.plot(time, dF_tot, 'r.')
+    pylab.ylim(min(dF_tot), max(dF_tot))
+    pylab.subplot(3,1,2)
+    pylab.ylim(min(lightcurve),max(lightcurve))
+    pylab.plot(time, lightcurve, 'b.')
+    pylab.subplot(3,1,3)
+    pylab.ylim(min(lightcurve),max(lightcurve))
+    dF_tot2 = dF_tot + lightcurve*Amplitude
+    pylab.plot(time, dF_tot, 'g.')
+    pylab.ylim(min(dF_tot), max(dF_tot))
+    
+    res1[0,:] = time
+    res1[1,:] = area_tot
+    res1[2,:] = dF_tot
+    res1[3,:] = dF_tot0
+
+    
+    print 'Done'
+    return res0, res1
+
+def runSim(amp = 0.01, N = 1, doplot = True, dur = 50, number_of_data_sets = 12, myperiod = [10.0], star_name = 1, spot_tau = 1, Amplitude = 1.0):
+    tau = spot_tau
+    diffrot = scipy.zeros(N) + 0.5
+    for q in range(0, number_of_data_sets):
+        for i in range(N): # One lightcurve per set of parameters
+            pars, ts = mklc(tau, diffrot[i], doplot = doplot, \
+                            dur = 50, myperiod = myperiod, quarter = q)
+            if doplot == True:
+                pylab.savefig('%s/%s/sim_%04dtest.png' % (ROOTDIR, (q+3), (star_name+1)))
+                scipy.io.savemat('%s/%s/orig_sim_%s.png' % (ROOTDIR, (q+3), (star_name+1)), \
+                                 {'pars': pars, 'ts': ts})
+            
+    return pars 
+
+# pars = [number of spots, filling factor, amplitude, noise], ts = [time, area_tot, dF_tot, dF_tot0] 
+
---
+++ ss_period_plots3.py
@@ -0,0 +1,211 @@
+# This script takes the Periods.txt files from various directories and compiles lists of targets and various measurements of period. It then makes a plot for each star with all the measurements of period. It also tells you whether you have made a reliable period measurement for each star. Additional - It will plot either all_data, individual quarters or yearly data. This is the false lc version. You should need the period finder because there are no missing stars
+
+#This version does the above for the fake star spot data.
+
+# This version is for individual quarters
+
+
+import numpy
+import scipy
+import pylab
+import atpy
+
+def period_extract(all_data = False, ind_quarters = False, years = False, no_stars = 24):
+
+    final_KID_list = []
+    ''' importing file of all KIDs'''
+    all_targs = range(0,no_stars)
+    #all_targs = ['01','02','03','04','05','06','07','08','09','10', ]
+    #all_targs = numpy.genfromtxt('/Users/angusr/Documents/rotation/all_targets.txt').T
+
+    ''' Extracting data from text files.'''
+    data3 = numpy.genfromtxt('/Users/angusr/angusr/ACF/PDCQss3_output/Periods_ss3test.txt').T
+    data4 = numpy.genfromtxt('/Users/angusr/angusr/ACF/PDCQss4_output/Periods_ss4test.txt').T    
+    data5 = numpy.genfromtxt('/Users/angusr/angusr/ACF/PDCQss5_output/Periods_ss5test.txt').T
+    data6 = numpy.genfromtxt('/Users/angusr/angusr/ACF/PDCQss6_output/Periods_ss6test.txt').T
+    data7 = numpy.genfromtxt('/Users/angusr/angusr/ACF/PDCQss7_output/Periods_ss7test.txt').T
+    data8 = numpy.genfromtxt('/Users/angusr/angusr/ACF/PDCQss8_output/Periods_ss8test.txt').T
+    data9 = numpy.genfromtxt('/Users/angusr/angusr/ACF/PDCQss9_output/Periods_ss9test.txt').T
+    data10 = numpy.genfromtxt('/Users/angusr/angusr/ACF/PDCQss10_output/Periods_ss10test.txt').T
+    data11 = numpy.genfromtxt('/Users/angusr/angusr/ACF/PDCQss11_output/Periods_ss11test.txt').T
+    data12 = numpy.genfromtxt('/Users/angusr/angusr/ACF/PDCQss12_output/Periods_ss12test.txt').T
+    data13 = numpy.genfromtxt('/Users/angusr/angusr/ACF/PDCQss13_output/Periods_ss13test.txt').T
+    data14 = numpy.genfromtxt('/Users/angusr/angusr/ACF/PDCQss14_output/Periods_ss14test.txt').T
+    # data_q36 = numpy.genfromtxt('/Users/angusr/angusr/ACF/PDCQss3-6_output/Periods_ss3-6test.txt').T
+    # data_q710 = numpy.genfromtxt('/Users/angusr/angusr/ACF/PDCQss7-10_output/Periods_ss7-10test.txt').T
+    # data_q1114 = numpy.genfromtxt('/Users/angusr/angusr/ACF/PDCQss11-14_output/Periods_ss11-14test.txt').T
+
+    KID3 = data3[0]; periods3 = data3[1]; errors3 = data3[2]; sine3 = data3[3]
+    KID4 = data4[0]; periods4 = data4[1]; errors4 = data4[2]; sine4 = data4[3]
+    KID5 = data5[0]; periods5 = data5[1]; errors5 = data5[2]; sine5 = data5[3]
+    KID6 = data6[0]; periods6 = data6[1]; errors6 = data6[2]; sine6 = data6[3]
+    KID7 = data7[0]; periods7 = data7[1]; errors7 = data7[2]; sine7 = data7[3]
+    KID8 = data8[0]; periods8 = data8[1]; errors8 = data8[2]; sine8 = data8[3]
+    KID9 = data9[0]; periods9 = data9[1]; errors9 = data9[2]; sine9 = data9[3]
+    KID10 = data10[0]; periods10 = data10[1]; errors10 = data10[2]; sine10 = data10[3]
+    KID11 = data11[0]; periods11 = data11[1]; errors11 = data11[2]; sine11 = data11[3]
+    KID12 = data12[0]; periods12 = data12[1]; errors12 = data12[2]; sine12 = data12[3]
+    KID13 = data13[0]; periods13 = data13[1]; errors13 = data13[2]; sine13 = data13[3]
+    KID14 = data14[0]; periods14 = data14[1]; errors14 = data14[2]; sine14 = data14[3]
+    # KIDq710 = data_q710[0]; periodsq710 = data_q710[1]; errorsq710 = data_q710[2]; sineq710 = data_q710[3]
+    # KIDq1114 = data_q1114[0]; periodsq1114 = data_q1114[1]; errorsq1114 = data_q1114[2]; sineq1114 = data_q1114[3]
+    # KIDq36 = data_q36[0]; periodsq36 = data_q36[1]; errorsq36 = data_q36[2]; sineq36 = data_q36[3]
+
+    print 'Found %d Targets in quarter 3' %len(KID3)
+    print 'Found %d Targets in quarter 4' %len(KID4)
+    print 'Found %d Targets in quarter 5' %len(KID5)
+    print 'Found %d Targets in quarter 6' %len(KID6)
+    print 'Found %d Targets in quarter 7' %len(KID7)
+    print 'Found %d Targets in quarter 8' %len(KID8)
+    print 'Found %d Targets in quarter 9' %len(KID9)
+    print 'Found %d Targets in quarter 10' %len(KID10)
+    print 'Found %d Targets in quarter 11' %len(KID11)
+    print 'Found %d Targets in quarter 12' %len(KID12)
+    print 'Found %d Targets in quarter 13' %len(KID13)
+    print 'Found %d Targets in quarter 14' %len(KID14)
+    # print 'Found %d Targets in quarters 3-6' %len(KIDq36)
+    # print 'Found %d Targets in quarters 7-10' %len(KIDq710)
+    # print 'Found %d Targets in quarters 11-14' %len(KIDq1114)
+
+    if all_data == True:
+        KID_method = [KID3, KID4, KID5, KID6, KID7, KID8, KID9, KID10, KID11, KID12, KID13, KID14, KIDq04, KIDq25, KIDq59, KIDq36, KIDq710, KIDq1114, KIDq09]
+        period_method = [periods3, periods4, periods5, periods6, periods7, periods8, periods9, periods10, periods11, periods12, periods13, periods14, periodsq04, periodsq25, periodsq59, periodsq36, periodsq710, periodsq1114, periodsq09]
+        error_method = [errors3, errors4, errors5, errors6, errors7, errors8, errors9 , errors10, errors11, errors12, errors13, errors14, errorsq04, errorsq25, errorsq59, errorsq36, errorsq710, errorsq1114, errorsq09]
+        sine_method = [sine3, sine4, sine5, sine6, sine7, sine8, sine9, sine10, sine11, sine12, sine13, sine14, sineq04, sineq25, sineq59, sineq36, sineq710, sineq1114, sineq09]  
+        KID_meth_string = ['Q3', 'Q4', 'Q5', 'Q6', 'Q7', 'Q8', 'Q9', 'Q10', 'Q11', 'Q12', 'Q13', 'Q14', 'Qs 1-4', 'Qs 2-5', 'Qs 5-9', 'Qs 3-6', 'Qs 7-10', 'Qs 11-14', 'Qs 1-8']
+        xtick_values = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
+        xtick_labels = ['3','4','5','6','7','8','9','10','11','12','13','14','1-4','2-5', '3-6', '5-9','7-10','11-14','1-8']
+        fig_dir = 'ss_all_data_figs'
+        
+    elif ind_quarters == True:
+        KID_method = [KID3, KID4, KID5, KID6, KID7, KID8, KID9, KID10, KID11, KID12, KID13, KID14]
+        period_method = [periods3, periods4, periods5, periods6, periods7, periods8, periods9, periods10, periods11, periods12, periods13, periods14]
+        error_method = [errors3, errors4, errors5, errors6, errors7, errors8, errors9 , errors10, errors11, errors12, errors13, errors14]
+        sine_method = [sine3, sine4, sine5, sine6, sine7, sine8, sine9, sine10, sine11, sine12, sine13, sine14]  
+        KID_meth_string = ['Q3', 'Q4', 'Q5', 'Q6', 'Q7', 'Q8', 'Q9', 'Q10', 'Q11', 'Q12', 'Q13', 'Q14']
+        xtick_values = [1,2,3,4,5,6,7,8,9,10,11,12]
+        xtick_labels = ['3','4','5','6','7','8','9','10','11','12','13','14']
+        fig_dir = 'ss_ind_qs_figs'
+
+    elif years == True:
+        KID_method = [KIDq36, KIDq710, KIDq1114]
+        period_method = [periodsq36, periodsq710, periodsq1114]
+        error_method = [errorsq36, errorsq710, errorsq1114]
+        sine_method = [sineq36, sineq710, sineq1114]  
+        KID_meth_string = ['Qs 3-6', 'Qs 7-10', 'Qs 11-14']
+        xtick_values = [1,2,3]
+        xtick_labels = ['3-6','7-10','11-14']
+        fig_dir = 'ss_year_figs'
+
+
+    '''Loop over all targets'''    
+    for i in range(0,len(all_targs)):
+        
+        if years == True:
+            periods = [periodsq36[i], periodsq710[i], periodsq1114[i]]
+            errors = [errorsq36[i], errorsq710[i], errorsq1114[i]]
+
+        elif ind_quarters == True:
+            periods = [periods3[i], periods4[i], periods5[i], periods6[i], periods7[i], periods8[i], periods9[i], periods10[i], periods11[i], \
+                       periods12[i], periods13[i], periods14[i]]
+            errors = [errors3[i], errors4[i], errors5[i], errors6[i], errors7[i], errors8[i], errors9[i], errors10[i], errors11[i], \
+                      errors12[i], errors13[i], errors14[i]]
+        
+        ''' Find medians '''
+        acf_median = numpy.median(periods)
+        
+        '''PLOTTING'''
+        KID1s = range(1,len(KID_method)+1)
+        pylab.figure(1)
+        pylab.clf()
+
+        
+        '''only plot if a period measurement was made. '''
+        for j in range(0,len(KID_method)):
+            #print j, len(periods)
+            if periods[j] > 0. and errors[j] > 0.:
+                pylab.errorbar(KID1s[j], periods[j], yerr = errors[j], marker = 'o', color = 'b', markersize = 5)
+            else: pylab.axvline(j+1, linewidth = 0.5, color = 'r', linestyle = '--')
+        pylab.ylabel('Period')
+        upper_y_lim = max(periods) + 5
+        pylab.xlim(0, len(KID_meth_string)+1)
+        pylab.ylim(0,upper_y_lim)
+
+        '''Adding harmonic lines'''
+        pylab.axhline(acf_median, linewidth = 0.5, color = 'b')
+        pylab.axhline(acf_median/2., linewidth = 0.5, color = 'b', linestyle = '--')
+        period_multiple = 0; harmonic=2
+        if acf_median != 0:
+            while period_multiple < upper_y_lim:
+                period_multiple = acf_median*harmonic
+                pylab.axhline(period_multiple, linewidth = 0.5, color = 'b', linestyle = '--')
+                harmonic += 1
+            
+        KID_string = numpy.int(i)
+        KID_string = numpy.str(KID_string)
+        pylab.title('%s' %KID_string)
+
+        #print periods
+        
+        '''Making sure that all measurements lie within 15% of the harmonic lines'''
+        number_of_measurements = 0
+        number_of_good_measurements = 0
+        margin = acf_median/7.5
+        for p in range(0,len(periods)):
+            if periods[p] > 0.0 and errors[p] > 0:
+                number_of_measurements += 1
+                if acf_median - margin < periods[p] < acf_median + margin:
+                    number_of_good_measurements += 1
+                elif acf_median*2.0 - margin < periods[p] < acf_median*2.0 + margin:
+                    number_of_good_measurements += 1
+                elif acf_median*3.0 - margin < periods[p] < acf_median*3.0 + margin:
+                    number_of_good_measurements += 1
+                elif acf_median*0.5 - margin < periods[p] < acf_median*0.5 + margin:
+                    number_of_good_measurements += 1
+        pylab.axhspan(acf_median - margin, acf_median + margin, facecolor='b', alpha=0.1)
+        pylab.axhspan(acf_median*2.0 - margin, acf_median*2.0 + margin, facecolor='b', alpha=0.1)
+        pylab.axhspan(acf_median*3.0 - margin, acf_median*3.0 + margin, facecolor='b', alpha=0.1)
+        pylab.axhspan(acf_median*0.5 - margin, acf_median*0.5 + margin, facecolor='b', alpha=0.1)
+
+        '''Requires that periods are measured in at least 2/3'''
+        if years == True:
+            relax = 1.0
+        else:
+            relax = 2./3.
+        if number_of_measurements < int((len(KID_meth_string))*relax): 
+            pylab.text(0,0, 'MISSING MEASUREMENTS, require %s/%s' %( int((len(KID_meth_string))*2./3.), len(KID_meth_string)))
+            pylab.axvspan(0, 120, facecolor='r', alpha=0.07)
+            selected = False
+            #require that 2/3 measurements are good!
+        else:
+            bonus = number_of_measurements - int((len(KID_meth_string))*relax) + 1
+            outliers = number_of_measurements - number_of_good_measurements
+            if outliers > bonus:
+                pylab.axvspan(0, 20, facecolor='r', alpha=0.07)
+                pylab.text(0,0, 'INCONSISTENT MEASUREMENTS, require < %s outliers' %(outliers - bonus + 3))
+                selected = False
+            else:
+                final_KID_list.append((numpy.float(KID_string))+1)
+                selected = True
+        
+          
+        pylab.xticks(xtick_values, xtick_labels)
+        print KID_string
+        #if years == True and selected == True:
+            #print max(periods) - min(periods)
+        pylab.savefig('/Users/angusr/angusr/ACF/%s/%s.pdf' %(fig_dir, KID_string))
+        # raw_input('enter')
+
+        '''The difference between newfigs2 and newfigs3 is that newfigs3 has data that has had the ACF code run on it more recently newfigs5 has all data, all_data_figs has all data, ind_qs_figs will have individual quarters only and year_figs will have just long term data'''
+
+    
+    print final_KID_list
+    print '%s targets selected' %len(final_KID_list)
+    if ind_quarters == True:
+        txt_tit = 'ss_ind_quarterstest'
+    elif years == True:
+        txt_tit = 'ss_yearstest'
+    elif all_data == True:
+        txt_tit = 'ss_all_datatest'
+    numpy.savetxt('/Users/angusr/angusr/ACF/star_spot_sim/%s.txt' %txt_tit, final_KID_list)
+    return
