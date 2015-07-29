b50ff64d63a1c06f624808d861e48d6092cfcccf
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2015-07-29 11:33:07 -0700

    tidying yp

--- M35/simple_acf.py
+++ M35/simple_acf.py
@@ -36,10 +36,9 @@ def dan_acf(x, axis=0, fast=False):
     m[axis] = 0
     return acf / acf[m]
 
-def process(x, y):
+def simple_acf(x, y):
 
-    # fit straight line
-    gap_days = 0.02043365
+    # fit and subtract straight line
     AT = np.vstack((x, np.ones_like(x)))
     ATA = np.dot(AT, AT.T)
     m, b = np.linalg.solve(ATA, np.dot(AT, y))
@@ -51,42 +50,57 @@ def process(x, y):
     # smooth with Gaussian kernel convolution
     Gaussian = lambda x, sig: 1./(2*np.pi*sig**.5) * \
                  np.exp(-0.5*(x**2)/(sig**2))
-    #create the smoothing kernel
     conv_func = Gaussian(np.arange(-28,28,1.), 9.)
     acf_smooth = np.convolve(acf, conv_func, mode='same')
 
     # create 'lags' array
+    gap_days = 0.02043365
     lags = np.arange(len(acf))*gap_days
-
-#     # trim acf
-#     trim = 20
-#     acf_smooth, lags = acf_smooth[trim:], lags[trim:]
 
     # find all the peaks
     peaks = np.array([i for i in range(1, len(lags)-1)
                      if acf_smooth[i-1] < acf_smooth[i] and
                      acf_smooth[i+1] < acf_smooth[i]])
 
-    # find the lag of highest correlation
-    m = acf_smooth ==  max(acf_smooth[peaks])
-    highest = lags[m]
+    # throw the first peak away
+    peaks = peaks[1:]
 
     # find the first and second peaks
     if acf_smooth[peaks[0]] > acf_smooth[peaks[1]]:
         period = lags[peaks[0]]
     else: period = lags[peaks[1]]
-    print period
 
-    plt.clf()
-    plt.subplot(2, 1, 1)
-    plt.plot(x, y, "k.")
-    plt.subplot(2, 1, 2)
-    for i in peaks:
-        plt.axvline(lags[i], color="r")
-    plt.axvline(highest, color="g")
-    plt.axvline(period, color="k")
-    plt.plot(lags, acf_smooth)
-    plt.show()
+    return period, acf_smooth, lags
+
+def make_plot(acf_smooth, lags, id):
+        # find all the peaks
+        peaks = np.array([i for i in range(1, len(lags)-1)
+                         if acf_smooth[i-1] < acf_smooth[i] and
+                         acf_smooth[i+1] < acf_smooth[i]])
+
+        # throw the first peak away
+        peaks = peaks[1:]
+
+        # find the lag of highest correlation
+        m = acf_smooth ==  max(acf_smooth[peaks])
+        highest = lags[m]
+
+        # find the first and second peaks
+        if acf_smooth[peaks[0]] > acf_smooth[peaks[1]]:
+            period = lags[peaks[0]]
+        else: period = lags[peaks[1]]
+        print period
+
+        plt.clf()
+        plt.subplot(2, 1, 1)
+        plt.plot(x, y, "k.")
+        plt.subplot(2, 1, 2)
+        for i in peaks:
+            plt.axvline(lags[i], color="r")
+        plt.axvline(highest, color="g")
+        plt.axvline(period, color="k")
+        plt.plot(lags, acf_smooth)
+        plt.savefig("%s_acf" % id)
 
 if __name__ == "__main__":
 
@@ -98,4 +112,5 @@ if __name__ == "__main__":
         x, y, _, _ = np.genfromtxt(fname, skip_header=1).T
         yerr = np.ones_like(y) * 1e-5  # FIXME
 
-        process(x, y)
+        period, acf, lags = simple_acf(x, y)
+        make_plot(acf, lags, id)
