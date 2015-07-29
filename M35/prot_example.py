a075bde54fa4e806478cb182a3314e9ecbc1e104
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2015-07-29 10:14:18 -0700

    running on whole set

--- M35/prot_example.py
+++ M35/prot_example.py
@@ -1,10 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from Kepler_ACF import corr_run
+import glob
 
-x, y, _, _ = np.genfromtxt("J06071008+2404374_xy_v2_ap2.0.dat",
-                           skip_header=1).T
+DIR = "."  # edit me!
+fnames = glob.glob("%s/*.dat" % DIR)
 
-plt.clf()
-plt.plot(x, y, "k.")
-plt.savefig("lc")
+for i, fname in enumerate(fnames):
+    id = fname.split("/")[-1].split("_")[0]  # edit me!
+    x, y, _, _ = np.genfromtxt(fname, skip_header=1).T
+    yerr = np.ones_like(y) * 1e-5  # FIXME
+    corr_run(x, y, yerr, id, "acf")
